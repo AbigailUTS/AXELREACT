48124e94b3d700158a9352ff779f6ed260c299b2
Abigail <612210073@utsalamanca.edu.mx>
2024-04-01 09:57:06 -0600

    FIRST COMMIT

---
+++ src/components/ChatWebSocket/ChatWebSocket.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import { Fisen } from "react-icons/fi"
+
+function ChatWebSocket(){
+    return(
+        <div className="chat">
+        <div className=""chat-header>
+        Titulo
+        </div>
+        {/* bucle que muestre mensajes */}
+        <div className="chat-message">
+        mensaje
+        </div>
+        <div className="chat-sender">
+        <input className="chat-send"></input>
+        {/* <Fisend/>*/}
+        {/* npm start */}
+        </div>
+
+    </div>
+    );
+}
+export default ChatWebSocket;
